f7b47e377cc9edfba43243c87070bb7d219f0f6d
Felipe Fuentes <felipe.c.fuentes.g@gmail.com>
2022-10-24 21:24:08 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Card from "./components/Card";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="App">
+        <Header titulo="Galería de Imagenes con React" />
+        <Container className="container">
+          <Row>
+            <Col><Card imagen='https://placedog.net/401' descripcion='Los Perros son tela!' titulo='Perro 1' /></Col>
+            <Col><Card imagen='https://placedog.net/402' descripcion='Los Perros son tela!' titulo='Perro 2' /></Col>
+            <Col><Card imagen='https://placedog.net/403' descripcion='Los Perros son tela!' titulo='Perro 3' /></Col>
+          </Row>
+        </Container>
+        <Container className="container">
+          <Row>
+            <Col><Card imagen='https://placedog.net/404' descripcion='Los Perros son tela!' titulo='Perro 4' /></Col>
+            <Col><Card imagen='https://placedog.net/405' descripcion='Los Perros son tela!' titulo='Perro 5' /></Col>
+            <Col><Card imagen='https://placedog.net/406' descripcion='Los Perros son tela!' titulo='Perro 6' /></Col>
+          </Row>
+        </Container>
+
+        <Footer />
+      </div>
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Badge from 'react-bootstrap/Badge';
+  
+
+
+const Footer = () => {
+    return (
+        <div>
+             <h4> perros@perros.com <Badge bg="secondary">2022.-</Badge></h4>
+        </div>
+        
+    )
+}  
+
+export default Footer;
